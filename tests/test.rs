e535b0a5e11e8eb9cfebf3ee96bceaac7c244658
Ilia Dorofeev <ilia.dorofeev@revolut.com>
2024-03-23 17:17:02 +0000

    hash docker build context

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,7 @@
-use std::{env, process::Command};
+use std::{env, ffi::OsString, process::Command};
 use colored::Colorize;
+use merkle_hash::{MerkleTree, Encodable};
+
 
 #[test]
 fn build_docker_image() {
@@ -12,6 +14,17 @@ fn build_docker_image() {
         .arg("-t")
         .arg("pg-browser/postgres")
         .arg(docker_build_context));
+}
+
+#[test]
+fn hash_docker_build_context() {
+    let tree = MerkleTree::builder(docker_build_context().into_string().unwrap()).build().unwrap();
+    println!("{}", tree.root.item.hash.to_hex_string());
+}
+
+fn docker_build_context() -> OsString {
+    let current_dir = env::current_dir().expect("current path");
+    current_dir.join("postgres").into_os_string()
 }
 
 fn run_command(command: &mut Command) {
