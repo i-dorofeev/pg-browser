7212f588609ab1a4903d9c72039b09e23db617c9
Ilia Dorofeev <ilia.dorofeev@revolut.com>
2024-04-07 22:30:09 +0100

    extracted root_dir_reader

---
+++ src/readers/root_dir_reader.rs
@@ -0,0 +1,119 @@
+use std::{
+    fs::{self, Metadata},
+    path::PathBuf,
+};
+
+pub struct PgDataItem {
+    pub name: &'static str,
+    pub description: &'static str,
+    pub item_type: PgDataItemType,
+    pub state: PgDataItemState,
+}
+
+pub enum PgDataItemState {
+    Present,
+    Missing,
+    Error(std::io::Error),
+}
+
+pub enum PgDataItemType {
+    File,
+    Dir,
+}
+
+impl PgDataItemType {
+    fn matches(&self, metadata: &Metadata) -> bool {
+        match self {
+            PgDataItemType::File => metadata.is_file(),
+            PgDataItemType::Dir => metadata.is_dir(),
+        }
+    }
+}
+
+pub struct RootDirReader<'a> {
+    pub pgdata: &'a PathBuf,
+}
+
+impl<'a> RootDirReader<'a> {
+    pub fn known_pgdata_items(&self) -> Vec<PgDataItem> {
+        vec![
+            self.pgdata_file("PG_VERSION", "Major version number of PostgreSQL"),
+            self.pgdata_dir("base", "Per-database directories"),
+            self.pgdata_file(
+                "current_logfiles",
+                "File recording the log file(s) currently written to by the logging collector",
+            ),
+            self.pgdata_dir("global", "Cluster-wide tables, such as pg_database"),
+            self.pgdata_dir("pg_commit_ts", "Transaction commit timestamp data"),
+            self.pgdata_dir(
+                "pg_dynshmem",
+                "Files used by the dynamic shared memory subsystem",
+            ),
+            self.pgdata_dir("pg_logical", "Status data for logical decoding"),
+            self.pgdata_dir(
+                "pg_multixact",
+                "Multitransaction status data (used for shared row locks)",
+            ),
+            self.pgdata_dir("pg_notify", "LISTEN/NOTIFY status data"),
+            self.pgdata_dir("pg_replslot", "Replication slot data"),
+            self.pgdata_dir(
+                "pg_serial",
+                "Information about committed serializable transactions",
+            ),
+            self.pgdata_dir("pg_snapshots", "Exported snapshots"),
+            self.pgdata_dir("pg_stat", "Permanent files for the statistics subsystem"),
+            self.pgdata_dir(
+                "pg_stat_tmp",
+                "Temporary files for the statistics subsystem",
+            ),
+            self.pgdata_dir("pg_subtrans", "Subtransaction status data"),
+            self.pgdata_dir("pg_tblspc", "Symbolic links to tablespaces"),
+            self.pgdata_dir("pg_twophase", "State files for prepared transactions"),
+            self.pgdata_dir("pg_wal", "WAL (Write Ahead Log) files"),
+            self.pgdata_dir("pg_xact", "Transaction commit status data"),
+            self.pgdata_file("postgresql.auto.conf", "A file used for storing configuration parameters that are set by ALTER SYSTEM"),
+            self.pgdata_file("postmaster.opts", "A file recording the command-line options the server was last started with"),
+            self.pgdata_file("postmaster.pid", "A lock file recording the current postmaster process ID (PID) and other running server data")
+        ]
+    }
+
+    fn pgdata_file(&self, name: &'static str, description: &'static str) -> PgDataItem {
+        self.pgdata_item(PgDataItemType::File, name, description)
+    }
+
+    fn pgdata_dir(&self, name: &'static str, description: &'static str) -> PgDataItem {
+        self.pgdata_item(PgDataItemType::Dir, name, description)
+    }
+
+    fn pgdata_item(
+        &self,
+        item_type: PgDataItemType,
+        name: &'static str,
+        description: &'static str,
+    ) -> PgDataItem {
+        let item_path = self.pgdata.join(name);
+        match fs::metadata(item_path) {
+            Ok(metadata) => {
+                let state = match &item_type.matches(&metadata) {
+                    true => PgDataItemState::Present,
+                    false => PgDataItemState::Missing,
+                };
+                PgDataItem {
+                    name,
+                    description,
+                    item_type,
+                    state,
+                }
+            }
+            Err(err) => PgDataItem {
+                name,
+                description,
+                item_type,
+                state: match err.kind() {
+                    std::io::ErrorKind::NotFound => PgDataItemState::Missing,
+                    _ => PgDataItemState::Error(err),
+                },
+            },
+        }
+    }
+}
--- src/root_handler.rs
+++ src/root_handler.rs
@@ -1,12 +1,11 @@
-use std::{
-    fs::{self, Metadata},
-    io::ErrorKind::NotFound,
-    path::PathBuf,
-};
+use std::path::PathBuf;
 
 use colored::{Color, Colorize};
 
-use crate::handler::{string_iter, Handler, StringIter, TermSize};
+use crate::{
+    handler::{string_iter, Handler, StringIter, TermSize},
+    readers::root_dir_reader::{PgDataItem, PgDataItemState, PgDataItemType, RootDirReader},
+};
 
 pub struct RootHandler {
     pub pgdata: PathBuf,
@@ -24,7 +23,10 @@ impl Handler for RootHandler {
     }
 
     fn handle<'a>(&self, term_size: &'a TermSize) -> StringIter<'a> {
-        let pgdata_items = self.known_pgdata_items();
+        let root_dir_reader = RootDirReader {
+            pgdata: &self.pgdata,
+        };
+        let pgdata_items = root_dir_reader.known_pgdata_items();
         let name_col_width = pgdata_items
             .iter()
             .map(|item| item.name.len())
@@ -32,7 +34,7 @@ impl Handler for RootHandler {
             .unwrap_or(0);
 
         Box::new(
-            self.known_pgdata_items()
+            pgdata_items
                 .into_iter()
                 .map(move |item| Self::format_pgdata_item(item, name_col_width, term_size.cols))
                 .map(|item_str| format!("\n{item_str}")),
@@ -41,88 +43,6 @@ impl Handler for RootHandler {
 }
 
 impl RootHandler {
-    fn known_pgdata_items(&self) -> Vec<PgDataItem> {
-        vec![
-            self.pgdata_file("PG_VERSION", "Major version number of PostgreSQL"),
-            self.pgdata_dir("base", "Per-database directories"),
-            self.pgdata_file(
-                "current_logfiles",
-                "File recording the log file(s) currently written to by the logging collector",
-            ),
-            self.pgdata_dir("global", "Cluster-wide tables, such as pg_database"),
-            self.pgdata_dir("pg_commit_ts", "Transaction commit timestamp data"),
-            self.pgdata_dir(
-                "pg_dynshmem",
-                "Files used by the dynamic shared memory subsystem",
-            ),
-            self.pgdata_dir("pg_logical", "Status data for logical decoding"),
-            self.pgdata_dir(
-                "pg_multixact",
-                "Multitransaction status data (used for shared row locks)",
-            ),
-            self.pgdata_dir("pg_notify", "LISTEN/NOTIFY status data"),
-            self.pgdata_dir("pg_replslot", "Replication slot data"),
-            self.pgdata_dir(
-                "pg_serial",
-                "Information about committed serializable transactions",
-            ),
-            self.pgdata_dir("pg_snapshots", "Exported snapshots"),
-            self.pgdata_dir("pg_stat", "Permanent files for the statistics subsystem"),
-            self.pgdata_dir(
-                "pg_stat_tmp",
-                "Temporary files for the statistics subsystem",
-            ),
-            self.pgdata_dir("pg_subtrans", "Subtransaction status data"),
-            self.pgdata_dir("pg_tblspc", "Symbolic links to tablespaces"),
-            self.pgdata_dir("pg_twophase", "State files for prepared transactions"),
-            self.pgdata_dir("pg_wal", "WAL (Write Ahead Log) files"),
-            self.pgdata_dir("pg_xact", "Transaction commit status data"),
-            self.pgdata_file("postgresql.auto.conf", "A file used for storing configuration parameters that are set by ALTER SYSTEM"),
-            self.pgdata_file("postmaster.opts", "A file recording the command-line options the server was last started with"),
-            self.pgdata_file("postmaster.pid", "A lock file recording the current postmaster process ID (PID) and other running server data")
-        ]
-    }
-
-    fn pgdata_file(&self, name: &'static str, description: &'static str) -> PgDataItem {
-        self.pgdata_item(PgDataItemType::File, name, description)
-    }
-
-    fn pgdata_dir(&self, name: &'static str, description: &'static str) -> PgDataItem {
-        self.pgdata_item(PgDataItemType::Dir, name, description)
-    }
-
-    fn pgdata_item(
-        &self,
-        item_type: PgDataItemType,
-        name: &'static str,
-        description: &'static str,
-    ) -> PgDataItem {
-        let item_path = self.pgdata.join(name);
-        match fs::metadata(item_path) {
-            Ok(metadata) => {
-                let state = match &item_type.matches(&metadata) {
-                    true => PgDataItemState::Present,
-                    false => PgDataItemState::Missing,
-                };
-                PgDataItem {
-                    name,
-                    description,
-                    item_type,
-                    state,
-                }
-            }
-            Err(err) => PgDataItem {
-                name,
-                description,
-                item_type,
-                state: match err.kind() {
-                    NotFound => PgDataItemState::Missing,
-                    _ => PgDataItemState::Error(err),
-                },
-            },
-        }
-    }
-
     fn format_pgdata_item(
         item: PgDataItem,
         name_col_width: usize,
@@ -177,33 +97,6 @@ impl RootHandler {
     fn split(str: &str, n: usize, size: usize) -> String {
         str.chars().skip(size * n).take(size).collect()
     }
-}
-
-enum PgDataItemState {
-    Present,
-    Missing,
-    Error(std::io::Error),
-}
-
-enum PgDataItemType {
-    File,
-    Dir,
-}
-
-impl PgDataItemType {
-    fn matches(&self, metadata: &Metadata) -> bool {
-        match self {
-            PgDataItemType::File => metadata.is_file(),
-            PgDataItemType::Dir => metadata.is_dir(),
-        }
-    }
-}
-
-struct PgDataItem {
-    name: &'static str,
-    description: &'static str,
-    item_type: PgDataItemType,
-    state: PgDataItemState,
 }
 
 struct AHandler {}
