56543c38f6f0a11dcb33b402c04401fa368ff470
Ilia Dorofeev <ilia.dorofeev@revolut.com>
2024-05-05 19:18:15 +0100

    db_dir_reader: added mod fork_segment_file_impl

--- src/readers/root_dir_readers/base_dir_readers/db_dir_reader.rs
+++ src/readers/root_dir_readers/base_dir_readers/db_dir_reader.rs
@@ -4,8 +4,6 @@ use std::{
 };
 
 use anyhow::{anyhow, Context, Error, Result};
-use once_cell::sync::Lazy;
-use regex::Regex;
 
 use crate::common::result_option::ResultOption;
 use crate::common::{PgOid, SimpleDirEntry};
@@ -31,6 +29,16 @@ pub struct ForkSegmentFile {
     pub fork_type: ForkType,
     pub oid: PgOid,
     pub segment_id: u16,
+}
+
+impl ForkSegmentFile {
+    fn create(oid: u32, fork_type: ForkType, segment_id: u16) -> Self {
+        ForkSegmentFile {
+            fork_type,
+            oid: PgOid(oid),
+            segment_id,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -102,42 +110,88 @@ fn pg_version_file(dir_entry: &DirEntry) -> Result<Option<DbDirItem>> {
     todo!()
 }
 
-static FORK_SEGMENT_FILE_REGEX: Lazy<Regex> =
-    regex_static::lazy_regex!(r"^([0-9]{1,10})(_(fsm|vm))?(\.([0-9]*))?$");
+mod fork_segment_file_impl {
+    use once_cell::sync::Lazy;
+    use regex::Regex;
 
-impl ForkSegmentFile {
-    fn try_parse(file_name: &str) -> Option<ForkSegmentFile> {
-        match FORK_SEGMENT_FILE_REGEX.captures(&file_name) {
-            None => None,
-            Some(captures) => {
-                let oid = captures
-                    .get(1)
-                    .map(|m| m.as_str())
-                    .and_then(PgOid::try_parse);
-                let fork_type = ForkType::try_parse(captures.get(3).map(|m| m.as_str()));
-                let segment_id = captures
-                    .get(5)
-                    .map_or("0", |m| m.as_str())
-                    .parse::<u16>()
-                    .map_or(None, Option::Some);
+    use crate::common::PgOid;
 
-                match (oid, fork_type, segment_id) {
-                    (Some(oid), Some(fork_type), Some(segment_id)) => Some(ForkSegmentFile {
-                        fork_type,
-                        oid,
-                        segment_id,
-                    }),
-                    _ => None,
+    use super::{ForkSegmentFile, ForkType};
+
+    static FORK_SEGMENT_FILE_REGEX: Lazy<Regex> =
+        regex_static::lazy_regex!(r"^([0-9]{1,10})(_(fsm|vm))?(\.([0-9]*))?$");
+
+    impl ForkSegmentFile {
+        pub fn try_parse(file_name: &str) -> Option<ForkSegmentFile> {
+            match FORK_SEGMENT_FILE_REGEX.captures(&file_name) {
+                None => None,
+                Some(captures) => {
+                    let oid = captures
+                        .get(1)
+                        .map(|m| m.as_str())
+                        .and_then(PgOid::try_parse);
+                    let fork_type = ForkType::try_parse(captures.get(3).map(|m| m.as_str()));
+                    let segment_id = captures
+                        .get(5)
+                        .map_or("0", |m| m.as_str())
+                        .parse::<u16>()
+                        .map_or(None, Option::Some);
+
+                    match (oid, fork_type, segment_id) {
+                        (Some(oid), Some(fork_type), Some(segment_id)) => Some(ForkSegmentFile {
+                            fork_type,
+                            oid,
+                            segment_id,
+                        }),
+                        _ => None,
+                    }
                 }
             }
         }
     }
 
-    fn create(oid: u32, fork_type: ForkType, segment_id: u16) -> Self {
-        ForkSegmentFile {
-            fork_type,
-            oid: PgOid(oid),
-            segment_id,
+    #[cfg(test)]
+    mod tests {
+        use pretty_assertions::assert_eq;
+        use rstest::rstest;
+
+        use super::{ForkSegmentFile, ForkType};
+
+        #[rstest]
+        #[case("12345", (12345, ForkType::Main, 0))]
+        #[case("12345.1", (12345, ForkType::Main, 1))]
+        #[case("12345_fsm", (12345, ForkType::FreeSpaceMap, 0))]
+        #[case("12345_fsm.2", (12345, ForkType::FreeSpaceMap, 2))]
+        #[case("12345_vm", (12345, ForkType::VisibilityMap, 0))]
+        #[case("12345_vm.3", (12345, ForkType::VisibilityMap, 3))]
+        fn parses_fork_segment_file(
+            #[case] file_name: &str,
+            #[case] expected: (u32, ForkType, u16),
+        ) {
+            // given
+            let (oid, fork_type, segment_id) = expected;
+            let expected = ForkSegmentFile::create(oid, fork_type, segment_id);
+
+            // when
+            let parsed = ForkSegmentFile::try_parse(file_name);
+
+            // then
+            assert_eq!(parsed, Some(expected));
+        }
+
+        #[rstest]
+        #[case("pg_filenode.map")]
+        #[case("PG_VERSION")]
+        #[case("12340_qqq")]
+        #[case("12341_qqq.1")]
+        #[case("12342_fsm.q")]
+        #[case("12343_vm.q")]
+        fn does_not_parse_as_fork_segment_file(#[case] file_name: &str) {
+            // when
+            let parsed = ForkSegmentFile::try_parse(file_name);
+
+            // then
+            assert_eq!(parsed, None);
         }
     }
 }
@@ -150,47 +204,5 @@ impl ForkType {
             Some("vm") => Some(ForkType::VisibilityMap),
             _ => None,
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use pretty_assertions::assert_eq;
-    use rstest::rstest;
-
-    use super::{ForkSegmentFile, ForkType};
-
-    #[rstest]
-    #[case("12345", (12345, ForkType::Main, 0))]
-    #[case("12345.1", (12345, ForkType::Main, 1))]
-    #[case("12345_fsm", (12345, ForkType::FreeSpaceMap, 0))]
-    #[case("12345_fsm.2", (12345, ForkType::FreeSpaceMap, 2))]
-    #[case("12345_vm", (12345, ForkType::VisibilityMap, 0))]
-    #[case("12345_vm.3", (12345, ForkType::VisibilityMap, 3))]
-    fn parses_fork_segment_file(#[case] file_name: &str, #[case] expected: (u32, ForkType, u16)) {
-        // given
-        let (oid, fork_type, segment_id) = expected;
-        let expected = ForkSegmentFile::create(oid, fork_type, segment_id);
-
-        // when
-        let parsed = ForkSegmentFile::try_parse(file_name);
-
-        // then
-        assert_eq!(parsed, Some(expected));
-    }
-
-    #[rstest]
-    #[case("pg_filenode.map")]
-    #[case("PG_VERSION")]
-    #[case("12340_qqq")]
-    #[case("12341_qqq.1")]
-    #[case("12342_fsm.q")]
-    #[case("12343_vm.q")]
-    fn does_not_parse_as_fork_segment_file(#[case] file_name: &str) {
-        // when
-        let parsed = ForkSegmentFile::try_parse(file_name);
-
-        // then
-        assert_eq!(parsed, None);
     }
 }
