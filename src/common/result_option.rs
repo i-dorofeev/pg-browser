ec53b050310e501c8518081741edff8e77117120
Ilia Dorofeev <ilia.dorofeev@revolut.com>
2024-05-05 19:10:55 +0100

    result_option test cases

--- src/common/result_option.rs
+++ src/common/result_option.rs
@@ -26,3 +26,51 @@ impl<T> ResultOption<T> for Result<Option<T>> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::{anyhow, Result};
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
+    use super::ResultOption;
+
+    #[rstest]
+    #[case(Ok(Some(1)), Ok(Some(2)), Ok(Some(1)))]
+    #[case(Ok(None), Ok(Some(2)), Ok(Some(2)))]
+    #[case(Ok(None), Ok(None), Ok(None))]
+    #[case(Ok(None), Err(anyhow!("error")), Err(anyhow!("error")))]
+    #[case(Err(anyhow!("error")), Ok(Some(2)), Err(anyhow!("error")))]
+    fn or_if_empty_test_cases(
+        #[case] v1: Result<Option<i32>>,
+        #[case] v2: Result<Option<i32>>,
+        #[case] expected: Result<Option<i32>>,
+    ) {
+        // when
+        let result = v1.or_if_empty(|| v2);
+
+        // then
+        assert_eq!(result.map_err(stringify), expected.map_err(stringify));
+    }
+
+    #[rstest]
+    #[case(Ok(Some(1)), Ok(2), Ok(1))]
+    #[case(Ok(None), Ok(2), Ok(2))]
+    #[case(Ok(None), Err(anyhow!("error")), Err(anyhow!("error")))]
+    #[case(Err(anyhow!("error")), Ok(2), Err(anyhow!("error")))]
+    fn otherwise_test_cases(
+        #[case] v1: Result<Option<i32>>,
+        #[case] v2: Result<i32>,
+        #[case] expected: Result<i32>,
+    ) {
+        // when
+        let result = v1.otherwise(|| v2);
+
+        // then
+        assert_eq!(result.map_err(stringify), expected.map_err(stringify));
+    }
+
+    fn stringify(err: anyhow::Error) -> String {
+        format!("{:?}", err)
+    }
+}
