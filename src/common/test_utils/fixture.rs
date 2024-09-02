abbd8d1121e698f33d4691190b22ee988007c908
Ilia Dorofeev <ilia.dorofeev@revolut.com>
2024-09-03 00:17:49 +0100

    added pgdata init synchronisation

--- src/common/test_utils/fixture.rs
+++ src/common/test_utils/fixture.rs
@@ -1,8 +1,12 @@
 use std::path::PathBuf;
 
+use fslock::LockFile;
 use rstest::fixture;
 
 #[fixture]
 pub fn pgdata() -> PathBuf {
+    let mut lock_file = LockFile::open("target/pgdata_init.lock").unwrap();
+    lock_file.lock().unwrap();
+
     super::pgdata::ensure_pgdata()
 }
